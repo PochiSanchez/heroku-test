8727efba2be3c2a543d03ccb392c7e0a694808a7
Pochi1991 <pochiwuxia1991@gmail.com>
2022-02-10 15:02:01 -0500

    testing simple backup by pg_dump command

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import os
 print("hello heroku")
 dbURI = os.environ.get("DATABASE_URL")
 print(dbURI)
+try:
+    os.system("pg_dump " + dbURI + " > mydb.sql")
+    print "Backup completed"
+except Exception as e:
+    print "!!Problem occured!!"
+    print e
 print("end")
