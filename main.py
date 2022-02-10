5981df3b3593e6b290db670b62628fc45204ce3b
Pochi1991 <pochiwuxia1991@gmail.com>
2022-02-10 15:56:57 -0500

    test7

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ print("hello heroku")
 dbURI = os.environ.get("DATABASE_URL")
 print(dbURI)
 try:
-    os.system("pg_dump -T public.\"Test2\" " + dbURI + " > mydb2.sql")
+    command = "pg_dump -T \"*2\" " + dbURI + " > mydb2.sql"
+    print(command)
+    os.system("pg_dump -T \"*2\" " + dbURI + " > mydb2.sql")
     print("Backup completed")
 except Exception as e:
     print("!!Problem occured!!")
