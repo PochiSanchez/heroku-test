d8e991ff70d764ae1258bb6914537aecf3560d51
Pochi1991 <pochiwuxia1991@gmail.com>
2022-02-10 15:29:55 -0500

    test4 backup all except Test2 table

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("hello heroku")
 dbURI = os.environ.get("DATABASE_URL")
 print(dbURI)
 try:
-    os.system("pg_dump -T Test2 --exclude-table=Test2 " + dbURI + " > mydb2.sql")
+    os.system("pg_dump --exclude-table=Test2 " + dbURI + " > mydb2.sql")
     print("Backup completed")
 except Exception as e:
     print("!!Problem occured!!")
