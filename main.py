6d11210a2b52398539a419003d82fb41a508a041
Pochi1991 <pochiwuxia1991@gmail.com>
2022-02-14 17:36:00 -0500

    test upload to box

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from boxsdk import BoxAPIException, Client, JWTAuth
+
 print("hello heroku")
 dbURI = os.environ.get("DATABASE_URL")
 print(dbURI)
@@ -8,6 +10,20 @@ try:
     os.system(command)
     # os.system("pg_dump -T '\"Test2\"' " + dbURI + " > mydb2.sql")
     print("Backup completed")
+
+    auth = JWTAuth(
+            client_id= os.environ.get("BOX_CLIENT_ID"),
+            client_secret=os.environ.get("BOX_CLIENT_SECRET")
+        )
+    auth.authenticate_instance()
+    client = Client(auth)
+    folder_id = '12345'
+    file_name = 'latest_without_to_excluede.sql'
+    stream = open('file.pdf', 'rb')
+
+    new_file = client.folder(folder_id).upload_stream(stream, file_name)
+    print(f'File "{new_file.name}" uploaded to Box with file ID {new_file.id}')
+
 except Exception as e:
     print("!!Problem occured!!")
     print(e)
