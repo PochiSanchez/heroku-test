8858e94d4803d8208957ad5580ab8dab6486752c
Pochi1991 <pochiwuxia1991@gmail.com>
2022-02-14 17:04:10 -0500

    test new command

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ print("hello heroku")
 dbURI = os.environ.get("DATABASE_URL")
 print(dbURI)
 try:
-    command = "pg_dump -T '\"Test2\"' " + dbURI + " > mydb2.sql"
-    # command = "pg_dump -T \"*t2\" " + dbURI + " > mydb2.sql"
+    command = "pg_dump --exclude-table-data to_exclude -O -x -f latest_without_to_excluede.sql -v " + dbURI
     print(command)
-    os.system("pg_dump -T '\"Test2\"' " + dbURI + " > mydb2.sql")
+    os.system(command)
+    # os.system("pg_dump -T '\"Test2\"' " + dbURI + " > mydb2.sql")
     print("Backup completed")
 except Exception as e:
     print("!!Problem occured!!")
